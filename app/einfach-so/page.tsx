a4bd8add3a020750f7f73e81768e3f7b9be91b07
Darian Gerent <darian.gerent19@gmail.com>
2024-11-30 20:56:10 +0100

    styling

--- app/einfach-so/page.tsx
+++ app/einfach-so/page.tsx
@@ -33,9 +33,16 @@ export default async function Page() {
 
     return (
         <div className={styles.einfachSoContainer}>
-            <h1>Soooo Ems ♥️</h1>
-            <p>Da du mir auch einen Adventskalender geschenkt hast, dachte ich, es wäre eine coole Idee, wenn ich meinen kleinen Informatiker raushängen lasse und dir auch was Kleines bastle. Du kannst die Türchen einsammeln und dann später bei mir zu Hause abholen ;)</p>
+            <h1>Soooo Ems <sup className={styles.footnoteRef}>1</sup></h1>
+            <p className={styles.description}>Da du mir auch einen Adventskalender geschenkt hast, dachte ich, es wäre eine coole Idee, wenn ich meinen
+                kleinen Informatiker raushängen lasse und dir auch was Kleines bastle. Du kannst die Türchen einsammeln
+                und dann später bei mir zu Hause abholen ;)</p>
             <Image src={"/toad.png"} alt={"Toad Showing Tongue"} width={571} height={571}/>
+            <footer className={styles.footnote}>
+                <p>
+                    <sup>1</sup> Meine Hauptmaus ♥️
+                </p>
+            </footer>
         </div>
     );
 }
